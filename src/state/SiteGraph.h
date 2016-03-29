7450660f0a3fc8efaa39cefa2ec54bb077c0b968
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2016-03-29 17:18:55 -0300

    - New clases (State, SiteGraph, Environment) to store the evaluation of AST.

---
+++ src/state/SiteGraph.h
@@ -0,0 +1,21 @@
+/*
+ * SiteGraph.h
+ *
+ *  Created on: Mar 23, 2016
+ *      Author: naxo
+ */
+
+#ifndef STATE_SITEGRAPH_H_
+#define STATE_SITEGRAPH_H_
+
+namespace state {
+
+class SiteGraph {
+public:
+	SiteGraph();
+	~SiteGraph();
+};
+
+} /* namespace state */
+
+#endif /* STATE_SITEGRAPH_H_ */
---
+++ src/state/State.cpp
@@ -0,0 +1,21 @@
+/*
+ * State.cpp
+ *
+ *  Created on: Mar 23, 2016
+ *      Author: naxo
+ */
+
+#include "State.h"
+
+namespace state {
+
+State::State() {
+	// TODO Auto-generated constructor stub
+
+}
+
+State::~State() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace ast */

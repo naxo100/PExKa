2714c7e5090b8c6a572fa635a1773f506a6a78d5
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2016-10-13 17:03:37 -0300

    - Some fixes in Signature and its subclasses.

--- src/pattern/Signature.h
+++ src/pattern/Signature.h
@@ -20,16 +20,21 @@ class Signature {
 public:
 	class Site;
 	class EmptySite;
-	class LabeledSite;
+	class LabelSite;
 	class FloatRangeSite;
 	class IntRangeSite;
-	union Value {
-		float f;
-		int i;
-		string s;
-	};
+	class Value;
+
 	Signature();
 	~Signature();
+
+	short addSite(const string &name);
+	short addSite(const string &name,const vector<string> &labels);
+	short addSite(const string &name,int min,int max);
+	short addSite(const string &name,float min,float max);
+
+	const Site& getSite(const short id) const;
+	const Site& getSite(const string &name) const;
 private:
 	int id;
 	string name;
@@ -37,32 +42,41 @@ private:
 	unordered_map<string,short> site_ids;
 };
 
+class Value {
+	union {
+		const float f;
+		const int i;
+		const string *s;
+		const short id;
+	};
+	enum {FLOAT,INT,STRING,ID} type;
+};
 
 class Signature::Site {
 	string name;
 
 public:
-	virtual bool isPossibleValue(Value val) = 0;
+	virtual bool isPossibleValue(const Value &val) = 0;
 	virtual ~Site();
 
 };
 
 class Signature::EmptySite : Site {
-	virtual bool isPossibleValue(Value val) override;
+	virtual bool isPossibleValue(const Value &val) override;
 };
-class Signature::LabeledSite : Site {
-	string labels;
+class Signature::LabelSite : Site {
+	vector<string> labels;
 	unordered_map<string,short> label_ids;
 
-	virtual bool isPossibleValue(Value val) override;
+	virtual bool isPossibleValue(const Value &val) override;
 };
 class Signature::FloatRangeSite : Site {
 	float min,max;
-	virtual bool isPossibleValue(Value val) override;
+	virtual bool isPossibleValue(const Value &val) override;
 };
 class Signature::IntRangeSite : Site {
 	int min,max;
-	virtual bool isPossibleValue(Value val) override;
+	virtual bool isPossibleValue(const Value &val) override;
 };
 
 } /* namespace ast */

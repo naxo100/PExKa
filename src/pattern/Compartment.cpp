8efa09b40006de7aa0af7163559ab61ae9dc63a3
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2016-11-11 16:15:36 -0300

    - Several new methods for class Comaprtment. - Several implementations of those methods.

--- src/pattern/Compartment.cpp
+++ src/pattern/Compartment.cpp
@@ -9,22 +9,77 @@
 
 namespace pattern {
 
+/*****************************************
+ *********** Class Compartment ***********
+ ****************************************/
+int Compartment::TOTAL_CELLS = 0;
 //template <short dims>
-Compartment::Compartment() {
-	// TODO Auto-generated constructor stub
-
+Compartment::Compartment(const std::string &nme,const std::vector<int> &dims,state::BaseExpression* vol)
+		:name(nme),dimensions(dims),volume(vol) {
+	firstCell = cellsCount;
+	cellsCount = 1;
+	for(unsigned int i = 0; i < dimensions.size(); i++)
+		cellsCount *= dimensions[i];
+	TOTAL_CELLS += cellsCount;
 }
 
 //template <short dims>
 Compartment::~Compartment() {
-	// TODO Auto-generated destructor stub
 }
 
-using namespace boost::numeric::ublas;
-using namespace std;
+short* Compartment::getCellIndex(int cell_id){
+	short* index = new short[dimensions.size()];
+	int factor = cellsCount;
+	for(unsigned int i = dimensions.size()-1; i >= 0 ;i--){
+		factor /= dimensions[i];
+		index[i] = cell_id/factor;
+		cell_id %= factor;
+	}
+	return index;
+}
 
+int Compartment::getCellId(const short* cell_index){
+	int factor = 1, id = 0;
+	for(unsigned int i = 0; i < dimensions.size(); i++){
+		id += factor*cell_index[i];
+		factor *= dimensions[i];
+	}
+	return id;
+}
+
+
+
+using namespace boost::numeric::ublas;
+
+//needed for CompExpression::solve()
  /* Matrix inversion routine.
  Uses lu_factorize and lu_substitute in uBLAS to invert a matrix */
+template<class T>
+bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse);
+
+
+/************** CompartmentExpr ***********/
+
+CompartmentExpr::CompartmentExpr(const Compartment& c,const std::list<state::BaseExpression*> &expr)
+	: comp(c),cellExpr(expr)
+{
+
+}
+
+void CompartmentExpr::solve(const std::vector<short> &cell_index,std::unordered_map<std::string,int>& var_values){
+	try{
+		vector<short,std::vector<short> > cell(cellExpr.size(),cell_index);
+		vector<short> result = cell + prod(b,inverseA);
+		for(unsigned int i = 0; i < result.size(); i++)
+			var_values[varOrder[i]] = result[i];
+	}
+	catch(std::exception &e){
+
+	}
+
+}
+
+
 template<class T>
 bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse)
 {
@@ -48,15 +103,6 @@ bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse)
 	lu_substitute(A, pm, inverse);
 
 	return true;
-}
-
-
-
-
-CompartmentExpr::CompartmentExpr(const Compartment& c,const std::list<state::BaseExpression*> &expr)
-	: comp(c),cellExpr(expr)
-{
-
 }
 
 
--- src/pattern/Compartment.h
+++ src/pattern/Compartment.h
@@ -20,29 +20,46 @@ namespace pattern {
 
 //template <short dims>
 class Compartment {
+	static int TOTAL_CELLS;
 	std::string name;
-	int* dimensions;//[dims];
-	int totalCells;
-	float volume;
+	std::vector<int> dimensions;//[dims];
+	int cellsCount;
+	state::BaseExpression* volume;
+
+	int firstCell;
 
 public:
-	Compartment();
+	Compartment(const std::string &name,const std::vector<int> &dimensions,state::BaseExpression* volume);
 	~Compartment();
 
-	int* getCellId(int id);
+	short* getCellIndex(int cell_id);
+	int getCellId(const short* cell_index);
+
+	int getFirstCellId();
+	int getLastCellId();
 };
 
 
 class CompartmentExpr {
 	const Compartment& comp;
 	const std::list<state::BaseExpression*> cellExpr;
-	boost::numeric::ublas::matrix<int> inverseA;
-	boost::numeric::ublas::vector<int> B;
+	const std::string* varOrder;
+	boost::numeric::ublas::matrix<short> inverseA;
+	boost::numeric::ublas::vector<short> b;
 public:
 	CompartmentExpr(const Compartment& c,const std::list<state::BaseExpression*> &expr);
 
+	/** \brief Replace var_values in expression and return a (list of) cell(s)
+	 *
+	 *
+	 */
 	std::list<int*> getCells(const std::unordered_map<std::string,int> &var_values);
-	std::unordered_map<std::string,int> getVarValues(const short* cell);
+
+	/** \brief Solve the implicit system for auxiliars in this expression
+	 * and return a map with the values for the auxiliars.
+	 *
+	 */
+	void solve(const std::vector<short> &cell_index,std::unordered_map<std::string,int> & vars);
 
 };
 

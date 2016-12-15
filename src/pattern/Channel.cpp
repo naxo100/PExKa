522149058c3f37f57f03d3a74df363d3301edb79
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2016-12-15 16:48:11 -0300

    - Implementation of the class. - Method getConnections() to evaluate expressions and expand to list of links. - Debug methods toString and printConnections()

--- src/pattern/Channel.cpp
+++ src/pattern/Channel.cpp
@@ -10,21 +10,72 @@
 
 namespace pattern {
 
-Channel::Channel(const std::string &nme,const state::BaseExpression* where,
-		const Compartment &src_comp,const Compartment &trgt_comp,
-		const std::list<state::BaseExpression*> &src,const std::list<state::BaseExpression*> &dest)
-	: name(nme),source(src_comp),target(trgt_comp),srcExpr(src),trgtExpr(dest)
-{
-	filter = dynamic_cast<const state::AlgExpression<bool>* >(where);
-
-}
+Channel::Channel(const std::string &nme,const CompartmentExpr& src,
+		const CompartmentExpr& trgt)
+	: name(nme),source(src),target(trgt),filter(nullptr) {}
 
 Channel::~Channel() {
 	// TODO Auto-generated destructor stub
 }
 
 
+const std::string& Channel::getName() const {
+	return name;
+}
+
+
+std::list< std::list< int > > Channel::getConnections() const{
+	std::vector<short> cell_index(source.getCompartment().getDimensions().size());
+	std::unordered_map<std::string,int> var_map;
+	std::list< std::list<int> > ret;
+	do{
+		//std::cout << Compartment::cellToString(cell_index) << source.getCompartment().getCellId(cell_index) << std::endl;
+		std::list<int> l;
+		l.push_back(source.getCompartment().getCellId(cell_index));
+		try{
+			source.solve(cell_index,var_map);
+		}catch(std::exception &e){
+			continue;
+		}
+		//for(auto &elem : var_map)
+		//	std::cout << elem.first << " -> " << elem.second << std::endl;
+		auto l2 = target.getCells(var_map);
+		if(l2.size() == 0)
+			continue;
+		l.insert(l.end(),l2.begin(),l2.end());
+		ret.push_back(l);
+	}
+	while(source.getCompartment().nextCell(cell_index));
+	return ret;
+}
+
+
+void Channel::setFilter(const state::BaseExpression *where){
+	if(filter)
+		delete filter;
+	filter = dynamic_cast<const state::AlgExpression<bool>* >(where);;
+}
 //TODO
 void Channel::setVarValues(std::map<std::string,int*> &var_values){}
+
+
+
+//DEBUG methods
+std::string Channel::toString(bool show_links) const{
+	std::string ret(name);
+	ret += ": from " + source.toString() + " to " + target.toString();
+	return ret;
+}
+
+void Channel::printConnections(const std::list<std::list<int> >& l) const{
+	for(std::list<std::list<int> >::const_iterator l_it = l.cbegin();l_it != l.cend(); l_it++){
+		std::cout << source.getCompartment().cellIdToString(l_it->front()) << "  ->\t";
+		for(std::list<int>::const_iterator int_it = ++(l_it->cbegin());int_it != l_it->cend(); int_it++)
+			std::cout << target.getCompartment().cellIdToString(*int_it) << "\t";
+		std::cout << std::endl;
+	}
+}
+
+
 
 } /* namespace pattern */
--- src/pattern/Channel.h
+++ src/pattern/Channel.h
@@ -12,6 +12,7 @@
 #include <vector>
 #include <list>
 #include <map>
+#include <utility>
 #include "Compartment.h"
 #include "../state/AlgExpression.h"
 
@@ -19,19 +20,26 @@ namespace pattern {
 
 class Channel {
 	std::string name;
-	const Compartment &source,&target;
-	const std::list<state::BaseExpression*> srcExpr,trgtExpr;
+	const CompartmentExpr &source,&target;
 	const state::AlgExpression<bool> *filter;
 	std::map<std::string,int*> varValues;
 
 
 public:
-	Channel(const std::string &name,const state::BaseExpression* where,
-			const Compartment &src_comp,const Compartment &trgt_comp,
-			const std::list<state::BaseExpression*> &src,const std::list<state::BaseExpression*> &dest);
+	Channel(const std::string &name,
+			const CompartmentExpr& src, const CompartmentExpr& trgt);
 	~Channel();
 
+	const std::string& getName() const;
+	void setFilter(const state::BaseExpression *where);
 	void setVarValues(std::map<std::string,int*> &var_values);
+
+
+	std::list<std::list<int> > getConnections() const;
+
+	//DEBUG methods
+	std::string toString(bool show_links = false) const;
+	void printConnections(const std::list<std::list<int> >& conn) const;
 };
 
 } /* namespace pattern */

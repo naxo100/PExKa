380814a872059aab772a8f5d68a5e45805e897b2
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2016-12-15 17:13:07 -0300

    - Implementation of classes Compartment and CompExpression. - Add debug methods.

--- src/pattern/Compartment.cpp
+++ src/pattern/Compartment.cpp
@@ -5,6 +5,8 @@
  *      Author: naxo
  */
 
+
+#include "../util/Exceptions.h"
 #include "Compartment.h"
 
 namespace pattern {
@@ -14,9 +16,9 @@ namespace pattern {
  ****************************************/
 int Compartment::TOTAL_CELLS = 0;
 //template <short dims>
-Compartment::Compartment(const std::string &nme,const std::vector<int> &dims,state::BaseExpression* vol)
+Compartment::Compartment(const std::string &nme,const std::vector<short> &dims,state::BaseExpression* vol)
 		:name(nme),dimensions(dims),volume(vol) {
-	firstCell = cellsCount;
+	firstCell = TOTAL_CELLS;
 	cellsCount = 1;
 	for(unsigned int i = 0; i < dimensions.size(); i++)
 		cellsCount *= dimensions[i];
@@ -27,18 +29,23 @@ Compartment::Compartment(const std::string &nme,const std::vector<int> &dims,sta
 Compartment::~Compartment() {
 }
 
-short* Compartment::getCellIndex(int cell_id){
-	short* index = new short[dimensions.size()];
+const std::string& Compartment::getName() const {
+	return name;
+}
+
+void Compartment::getCellIndex(int cell_id,std::vector<short>& index) const{
+	if(index.size() != dimensions.size())
+		index = std::vector<short>(dimensions.size());
 	int factor = cellsCount;
-	for(unsigned int i = dimensions.size()-1; i >= 0 ;i--){
+	unsigned i = dimensions.size();
+	while(i--){
 		factor /= dimensions[i];
 		index[i] = cell_id/factor;
 		cell_id %= factor;
 	}
-	return index;
 }
 
-int Compartment::getCellId(const short* cell_index){
+int Compartment::getCellId(const std::vector<short> &cell_index) const{
 	int factor = 1, id = 0;
 	for(unsigned int i = 0; i < dimensions.size(); i++){
 		id += factor*cell_index[i];
@@ -47,6 +54,21 @@ int Compartment::getCellId(const short* cell_index){
 	return id;
 }
 
+const std::vector<short>& Compartment::getDimensions() const{
+	return dimensions;
+}
+
+bool Compartment::nextCell(std::vector<short>& cell) const {
+	for(unsigned int i = 0; i < dimensions.size(); i++)
+		if(cell[i] < dimensions[i]-1){
+			cell[i]++;
+			return true;
+		}
+		else{
+			cell[i] = 0;
+		}
+	return false;
+}
 
 
 using namespace boost::numeric::ublas;
@@ -61,22 +83,129 @@ bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse);
 /************** CompartmentExpr ***********/
 
 CompartmentExpr::CompartmentExpr(const Compartment& c,const std::list<state::BaseExpression*> &expr)
-	: comp(c),cellExpr(expr)
+	: comp(c),cellExpr(expr),b(expr.size())
 {
+	if(c.getDimensions().size() < cellExpr.size())
+		throw SemanticError("Compartment "+comp.getName()+" has not enough dimensions.");
+}
+
+
+void CompartmentExpr::cellIds(std::list<int>& cell_list,
+		std::list<short> *cell_values,std::vector<short> &cell_index,unsigned int dim) const{
+	if(dim == comp.getDimensions().size())
+		cell_list.push_back(comp.getCellId(cell_index));
+	else {
+		for(std::list<short>::iterator it = cell_values[dim].begin(); it != cell_values[dim].end();it++){
+			cell_index[dim] = *it;
+			cellIds(cell_list,cell_values,cell_index,dim+1);
+		}
+	}
+}
+
+const Compartment& CompartmentExpr::getCompartment() const{
+	return comp;
+}
+
+std::list<int> CompartmentExpr::getCells(const std::unordered_map<std::string,int> &var_values) const{
+	std::list<int> ret;
+	unsigned int dim = 0;
+	std::list<short> *cell_values = new std::list<short>[comp.getDimensions().size()];
+	for(std::list<state::BaseExpression*>::const_iterator it = cellExpr.cbegin();it != cellExpr.cend();it++){
+		try{
+			int index = (*it)->getValue(&var_values).iVal;
+			if(index < 0 || index >= comp.getDimensions()[dim])
+				return ret;
+			cell_values[dim].push_back(index);
+		}
+		catch(const std::out_of_range &e){
+			std::unordered_map<std::string,float> factors;
+			float b = (*it)->auxFactors(factors);
+			for(std::unordered_map<std::string,int>::const_iterator m_it = var_values.cbegin();
+					m_it != var_values.cend(); m_it++){
+				//maybe if factors[s] = 0 for none then remove '?' TODO
+				b -= m_it->second * (factors.count(m_it->first) ? factors[m_it->first] : 0);
+				factors.erase(m_it->first);
+			}
+			if(factors.size() > 1)
+				throw std::exception();//Cannot solve this equation
+			for(int i = 0; i < comp.getDimensions()[dim];i++){
+				float value = (b + i) / (float)factors.begin()->second;
+				if(value == (int)value)
+					cell_values[dim].push_back(i);
+			}
+		}
+		catch(std::exception &e){
+			std::cout << e.what() << std::endl;
+		}
+		dim++;
+	}
+	for(unsigned int i = dim; i < comp.getDimensions().size(); i++)
+		for(short index = 0; index < comp.getDimensions()[i]; index++)
+			cell_values[i].push_back(index);
+
+	//short *cell_index = new short[comp.getDimensions().size()];
+	std::vector<short> cell_index(comp.getDimensions().size());
+
+	cellIds(ret,cell_values,cell_index,0);
+	delete[] cell_values;
+	return ret;
+}
+
+
+void CompartmentExpr::solve(const std::vector<short> &cell_index,std::unordered_map<std::string,int>& var_values) const{
+	try{
+		vector<short,std::vector<short> > cell(cellExpr.size(),cell_index);
+		vector<float> b_;
+		if(cellExpr.size() != inverseA.size1())
+			b_ = prod(transA,b+cell);
+		else
+			b_ = (b + cell);
+		vector<float> result = prod(inverseA,b_);
+		for(unsigned int i = 0; i < result.size(); i++)
+			if(result[i] == (int)result[i])
+				var_values[varOrder[i]] = result[i];
+			else
+				throw std::exception();
+	}
+	catch(std::exception &e){
+		//std::cout << "an exception in solve()!!\n" << e.what() << std::endl;
+		throw SemanticError("Cannot solve the implicit equation system for this expression.");
+	}
 
 }
 
-void CompartmentExpr::solve(const std::vector<short> &cell_index,std::unordered_map<std::string,int>& var_values){
-	try{
-		vector<short,std::vector<short> > cell(cellExpr.size(),cell_index);
-		vector<short> result = cell + prod(b,inverseA);
-		for(unsigned int i = 0; i < result.size(); i++)
-			var_values[varOrder[i]] = result[i];
+void CompartmentExpr::setEquation(){
+	//get factors, initialize b and var_order
+	std::set<std::string> var_names;
+	int j = 0;
+	auto *var_factors = new std::unordered_map<std::string,float>[cellExpr.size()];
+	for(std::list<state::BaseExpression*>::const_iterator it = cellExpr.cbegin();it != cellExpr.cend();it++){
+		b[j] = -(*it)->auxFactors(var_factors[j]);
+		for(auto var_value : var_factors[j])
+			var_names.insert(var_value.first);
+		j++;
 	}
-	catch(std::exception &e){
-
+	varOrder = std::vector<std::string>(var_names.begin(),var_names.end());
+	//initialize A
+	j = 0;
+	matrix<float> A(cellExpr.size(),varOrder.size());
+	for(std::list<state::BaseExpression*>::const_iterator it = cellExpr.cbegin();it != cellExpr.cend();it++){
+		for(unsigned int i = 0; i < varOrder.size(); i++)
+			A(j,i) = var_factors[j][varOrder[i]];
+		j++;
 	}
+	if(A.size1() > A.size2()){//if there are more equations than vars
+		transA = matrix<float>(trans(A));
+		A = prod(transA,A);
+		//b = prod(At,b);
+	}
+	else if (A.size1() < A.size2())//if there are more vars than eqs
+		throw SemanticError("Cannot solve the implicit equation system for this expression.");
 
+	inverseA = matrix<float>(A.size1(),A.size2());
+	bool invertible = InvertMatrix(A,inverseA);
+	if(!invertible)
+		throw SemanticError("Cannot solve the implicit equation system for this expression.");
 }
 
 
@@ -104,6 +233,38 @@ bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse)
 
 	return true;
 }
+
+
+
+
+
+//DEBUG methods
+std::string Compartment::toString() const {
+	std::string ret(name);
+	for(unsigned int i = 0; i < dimensions.size(); i++)
+		ret += std::string("[")+std::to_string(dimensions[i])+"]";
+	return ret;
+}
+std::string Compartment::cellToString(const std::vector<short>& cell){
+	std::string ret;
+	for(unsigned i = 0; i < cell.size(); i++)
+		ret = ret + "[" + std::to_string(cell[i]) + "]";
+	return ret;
+}
+std::string Compartment::cellIdToString(int cell_id) const{
+	std::vector<short> cell;
+	getCellIndex(cell_id,cell);
+	return cellToString(cell);
+}
+
+std::string CompartmentExpr::toString() const {
+	std::string ret(comp.getName());
+	for(std::list<state::BaseExpression*>::const_iterator it = cellExpr.cbegin(); it != cellExpr.cend(); it++)
+		break;//ret += it->toString();
+	return ret;
+}
+
+
 
 
 } /* namespace pattern */
--- src/pattern/Compartment.h
+++ src/pattern/Compartment.h
@@ -22,45 +22,77 @@ namespace pattern {
 class Compartment {
 	static int TOTAL_CELLS;
 	std::string name;
-	std::vector<int> dimensions;//[dims];
+	std::vector<short> dimensions;//[dims];
 	int cellsCount;
 	state::BaseExpression* volume;
 
 	int firstCell;
 
 public:
-	Compartment(const std::string &name,const std::vector<int> &dimensions,state::BaseExpression* volume);
+	Compartment(const std::string &name,const std::vector<short> &dimensions,state::BaseExpression* volume);
 	~Compartment();
 
-	short* getCellIndex(int cell_id);
-	int getCellId(const short* cell_index);
+	const std::string& getName() const;
+
+	void getCellIndex(int cell_id,std::vector<short> &index) const;
+	int getCellId(const std::vector<short> &cell_index) const;
 
 	int getFirstCellId();
 	int getLastCellId();
+
+	bool nextCell(std::vector<short>& cell) const;
+	const std::vector<short>& getDimensions() const;
+
+
+	//DEBUG methods
+	std::string toString() const;
+	std::string cellIdToString(int cell_id) const;
+	static std::string cellToString(const std::vector<short>& cell);
+};
+
+
+struct Cell {
+	const Compartment &comp;
+	const int cellId;
+
+	Cell(const Compartment &c,int n);
+
+	std::string toString() const;
 };
 
 
 class CompartmentExpr {
 	const Compartment& comp;
 	const std::list<state::BaseExpression*> cellExpr;
-	const std::string* varOrder;
-	boost::numeric::ublas::matrix<short> inverseA;
-	boost::numeric::ublas::vector<short> b;
+	std::vector<std::string> varOrder;
+	boost::numeric::ublas::matrix<float> inverseA,transA;
+	boost::numeric::ublas::vector<float> b;
+
+	void cellIds(std::list<int>& cell_list,
+			std::list<short> *cell_values,std::vector<short> &cell_index,unsigned int dim) const;
 public:
 	CompartmentExpr(const Compartment& c,const std::list<state::BaseExpression*> &expr);
+
+
+	//bool nextCell(std::vector<short>& cell) const;
+	const Compartment& getCompartment() const;
 
 	/** \brief Replace var_values in expression and return a (list of) cell(s)
 	 *
 	 *
 	 */
-	std::list<int*> getCells(const std::unordered_map<std::string,int> &var_values);
+	std::list<int> getCells(const std::unordered_map<std::string,int> &var_values) const;
 
 	/** \brief Solve the implicit system for auxiliars in this expression
 	 * and return a map with the values for the auxiliars.
 	 *
 	 */
-	void solve(const std::vector<short> &cell_index,std::unordered_map<std::string,int> & vars);
+	void solve(const std::vector<short> &cell_index,std::unordered_map<std::string,int> & vars) const;
 
+	void setEquation();
+
+
+	std::string toString() const;
 };
 
 } /* namespace pattern */

4a568deb68a60e3f1d971494322682d8a5183157
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2017-02-01 14:51:44 -0300

    - SemanticError now uses char* string.

--- src/util/Exceptions.cpp
+++ src/util/Exceptions.cpp
@@ -7,17 +7,26 @@
 
 
 #include <sstream>
+#include <cstring>
 #include "Exceptions.h"
 
 using namespace std;
 
 SemanticError::SemanticError(const string &str,const yy::location &l)
-		: msg(str), loc(l) {}
+		: msg(), loc(l) {
+	strcpy(msg,str.c_str());
+}
 
 const char* SemanticError::what() const _GLIBCXX_USE_NOEXCEPT {
-	stringstream r;
-	r << "Semantic error: " << msg << "(" << loc << ")";
-	return r.str().c_str();
+	static char c[150];//TODO??
+	sprintf(c,"Semantic error in file \"%s\", line %d, characters %d-%d:\n%s",
+			loc.begin.filename->c_str(),loc.begin.line,loc.begin.column,loc.end.column,msg);
+	//stringstream r;
+	//string s (string("Semantic error in file \"") + *loc.begin.filename +
+	//		"\", line " + to_string(loc.begin.line) + ", characters " +
+	//		to_string(loc.begin.column) + "-" + to_string(loc.end.column) + ":\n" +
+	//		msg );
+	return c;
 }
 
 void SemanticError::setLocation(const yy::location &l){

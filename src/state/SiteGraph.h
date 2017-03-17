7bec42c05f08989297df50d7907c5aadf2a02e3d
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2017-03-17 14:40:46 -0300

    - Definition of Node and Internal classes.

--- src/state/SiteGraph.h
+++ src/state/SiteGraph.h
@@ -8,13 +8,40 @@
 #ifndef STATE_SITEGRAPH_H_
 #define STATE_SITEGRAPH_H_
 
+#include <utility> //pair
+
 namespace state {
+
+using namespace std;
 
 class SiteGraph {
 public:
+	class Node;
+	struct Internal;
 	SiteGraph();
 	~SiteGraph();
 };
+
+
+struct SiteGraph::Internal {
+	union {
+		short lVal;
+		int iVal;
+		float fVal;
+	};
+
+	pair<short,short> link;
+};
+
+class SiteGraph::Node {
+	short name;
+	long address;
+	Internal *interface;
+};
+
+
+
+
 
 } /* namespace state */
 

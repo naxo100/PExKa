c5f693c4afd8d14b082938255dc3e96662b281c4
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2017-05-17 17:01:27 -0400

    - header file with params for whole program.

---
+++ src/util/params.h
@@ -0,0 +1,14 @@
+
+#ifndef PARAMS_H
+#define PARAMS_H
+
+typedef unsigned long big_id;
+typedef unsigned int mid_id;
+typedef unsigned short short_id;
+typedef unsigned char small_id;
+
+typedef unsigned int pop_size;
+
+
+
+#endif

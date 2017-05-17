e74797933da7c3b85d13fecd617b8be0c94bb195
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2017-05-17 17:01:57 -0400

    - New class simulation::Rule

---
+++ src/simulation/Rule.cpp
@@ -0,0 +1,27 @@
+/*
+ * Rule.cpp
+ *
+ *  Created on: May 9, 2017
+ *      Author: naxo
+ */
+
+#include "Rule.h"
+
+namespace simulation {
+
+/*Rule::Rule() {
+	// TODO Auto-generated constructor stub
+
+}*/
+
+Rule::~Rule() {
+	// TODO Auto-generated destructor stub
+}
+
+
+
+list<Rule::Action> Rule::difference(const Mixture& lhs,const Mixture& rhs){
+	//for(auto)
+}
+
+} /* namespace simulation */

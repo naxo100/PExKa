784e226a7742c8788646235e6241e9acd0f291a6
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2017-10-03 12:55:33 -0300

    - Implementation of CcInjection constructor.

--- src/matching/Injection.h
+++ src/matching/Injection.h
@@ -8,45 +8,51 @@
 #ifndef SRC_MATCHING_INJECTION_H_
 #define SRC_MATCHING_INJECTION_H_
 
-#include "../util/params.h"
 #include <map>
 #include <set>
+#include <vector>
+#include "../util/params.h"
+#include "../state/SiteGraph.h"
+#include "../pattern/Mixture.h"
 
+
+namespace matching {
 
 using namespace std;
-namespace matching {
+
+using Node = state::SiteGraph::Node;
 
 class Injection {
 public:
-
-	virtual ~Injection() = 0;
-	virtual const map<small_id,big_id>& getEmbedding() const = 0;
+	//virtual ~Injection() = 0;
+	virtual const vector<Node*>& getEmbedding() const = 0;
 
 	virtual bool isTrashed() const = 0;
 
-	virtual void codomain(big_id *injs[],set<big_id> cod) const = 0;
+	virtual void codomain(Node* injs[],set<Node*> cod) const = 0;
 
 	bool operator< (const Injection& inj) const;
 };
 
-class CcInjection : Injection {
+class CcInjection : public Injection {
 	//map<small_id,big_id> ccAgToNode;//unordered
-	big_id* ccAgToNode;
+	vector<state::SiteGraph::Node*> ccAgToNode;
 	mid_id address;
 	small_id coordinate;//cc_id
 
 public:
-	CcInjection(small_id size);
+	CcInjection(const pattern::Mixture::Component& cc,state::SiteGraph::Node& node,
+			two<list<state::SiteGraph::Internal*> >& port_list);
 	~CcInjection();
 
-	const map<small_id,big_id>& getEmbedding() const override;
+	const vector<Node*>& getEmbedding() const override;
 
 	bool isTrashed() const override;
 
-	void codomain(big_id *injs[],set<big_id> cod) const override;
+	void codomain(Node* injs[],set<Node*> cod) const override;
 };
 
-class MixInjection : Injection {
+/*class MixInjection : Injection {
 	big_id** agCcIdToNode;
 	mid_id address;
 	small_id coordinate;//mix_id
@@ -55,12 +61,12 @@ public:
 	MixInjection(small_id size);
 	~MixInjection();
 
-	const map<small_id,big_id>& getEmbedding() const override;
+	const vector<Node*>& getEmbedding() const override;
 
 	bool isTrashed() const override;
 
-	void codomain(big_id *injs[],set<big_id> cod) const override;
-};
+	void codomain(Node* injs[],set<Node*> cod) const override;
+};*/
 
 //TODO choose the best implementation of InjSet
 class InjSet : public set<Injection*> {

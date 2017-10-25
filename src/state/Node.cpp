fb3a205959c8921c205749989dca1ef26719109c
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2017-10-25 16:25:26 -0300

    - Implementation of classes MultiNode and SubNode : Node.

---
+++ src/state/Node.cpp
@@ -0,0 +1,382 @@
+/*
+ * Node.cpp
+ *
+ *  Created on: Oct 18, 2017
+ *      Author: naxo
+ */
+
+#include <cstring>
+#include "Node.h"
+#include "SiteGraph.h"
+#include "../matching/Injection.h"
+#include "../pattern/Environment.h"
+
+namespace state {
+
+
+
+/********* Node Class ************/
+Node::Node(const pattern::Signature& sign) : signId(sign.getId()),address(-1), intfSize(sign.getSiteCount()){
+	interface = new Internal[intfSize];
+	for(int i = 0; i < intfSize; i++){
+		interface[i].val = sign.getSite(i).getDefaultValue();
+	}
+}
+
+Node::Node(const Node& node,map<Node*,Node*>& mask) : signId(node.signId),address(-1),intfSize(node.intfSize) {
+	interface = new Internal[intfSize];
+	for(small_id i = 0; i < intfSize; i++){
+		interface[i].val = node.interface[i].val;
+		try{
+			auto lnk = mask.at(node.interface[i].link.first);//maybe operator[]
+			if(lnk){
+				interface[i].link.first = lnk;
+				interface[i].link.second = node.interface[i].link.second;
+				lnk->interface[interface[i].link.second].link.first = this;
+				lnk->interface[interface[i].link.second].link.second = i;
+			}
+		}
+		catch(std::out_of_range &ex){
+			throw invalid_argument("Calling copy constructor with an incomplete mask");
+		}
+	}
+}
+
+Node::~Node() {
+	delete[] interface;
+}
+
+void Node::alloc(big_id addr){
+	address = addr;
+}
+
+big_id Node::getAddress() const {
+	return address;
+}
+
+void Node::setCount(pop_size q){
+	throw invalid_argument("Cannot set population of basic Node");
+}
+
+pop_size Node::getCount() const {
+	return 1;
+}
+
+short_id Node::getId() const{
+	return signId;
+}
+
+bool Node::test(const pair<small_id,pattern::Mixture::Site>& id_site,
+		std::queue<pair<small_id,Node&> >& match_queue,
+		two<list<Internal*> > &port_list) const{
+	auto& port = interface[id_site.first];
+	if(!id_site.second.isEmptySite()){
+		if(memcmp(&port.val,&id_site.second.state,sizeof(state::SomeValue)))
+			throw False();
+		else
+			port_list.first.emplace_back(&port);
+	}
+	switch(id_site.second.link_type){
+	case pattern::Mixture::WILD:
+		break;
+	case pattern::Mixture::FREE:
+		if(port.link.first)
+			throw False();
+		//else
+		port_list.second.emplace_back(&port);
+		break;
+	case pattern::Mixture::BIND:
+		if(!port.link.first)
+			throw False();//or return port.link.first;
+		//else
+		port_list.second.emplace_back(&port);
+		match_queue.emplace(id_site.first,*(port.link.first));//its site_id instead ag_mix_id
+		return true;
+		break;
+	case pattern::Mixture::BIND_TO:
+		if(port.link.first){
+			if(port.link.first->getId() != id_site.second.lnk_ptrn.first
+					|| port.link.second != id_site.second.lnk_ptrn.second)
+				throw False();
+		} else
+			throw False();
+		port_list.second.emplace_back(&port);
+		break;
+	case pattern::Mixture::PATH:
+		//TODO
+		break;
+	}
+	return false;
+}
+
+
+template <typename T>
+void Node::setState(small_id site_id,T value){
+	throw std::invalid_argument("Node::setState(): not a valid state value.");
+}
+
+template <> void Node::setState(small_id site_id,SomeValue value){
+	interface[site_id].val = value;
+}
+template <> void Node::setState(small_id site_id,small_id value){
+	interface[site_id].val.smallVal = value;
+}
+template <> void Node::setState(small_id site_id,float value){
+	interface[site_id].val.fVal = value;
+}
+template <> void Node::setState(small_id site_id,int value){
+	interface[site_id].val.iVal = value;
+}
+
+void Node::setLink(small_id site_src,Node* lnk,small_id site_trgt){
+	interface[site_src].link.first = lnk;
+	interface[site_src].link.second = site_trgt;
+}
+
+
+
+void Node::removeFrom(EventInfo& ev,matching::InjSet* injs,SiteGraph& graph) {
+	graph.remove(this);
+	for(small_id i = 0; i < intfSize; i++){
+		interface[i].negativeUpdate(ev,injs);
+		auto lnk = interface[i].link;
+		if(lnk.first){
+			lnk.first->interface[lnk.second].negativeUpdate(ev,injs,lnk.first->interface[lnk.second].deps.second);
+			lnk.first->setLink(lnk.second,nullptr,0);
+			ev.side_effects.emplace(lnk);
+		}
+	}
+	delete this;//TODO do not delete, reuse nodes
+}
+
+void Node::changeIntState(EventInfo& ev,matching::InjSet* injs,small_id id,small_id value){
+	if(interface[id].val.smallVal == value){
+		ev.warns++;
+	}
+	else{
+		interface[id].val.set(value);
+		interface[id].negativeUpdate(ev,injs,interface[id].deps.first);
+	}
+}
+void Node::unbind(EventInfo& ev,matching::InjSet* injs,small_id id){
+	auto& inter = interface[id];
+	if(inter.link.first){
+		inter.link.first->interface[inter.link.second].negativeUpdate(ev,//?? private?
+				inter.link.first->interface[inter.link.second].deps.second);
+		inter.link.first->setLink(inter.link.second,nullptr,0);
+		inter.negativeUpdate(ev,injs,inter.deps.second);
+		inter.link.first = nullptr;
+	}
+	else
+		ev.warns++;
+}
+void Node::bind(EventInfo& ev,matching::InjSet* injs,small_id id,Node* trgt_node,small_id trgt_site){
+	auto& lnk2 = trgt_node->getLinkState(trgt_site);
+	auto& lnk1 = interface[id].link;
+	if(lnk1.first){
+		lnk1.first->interface[lnk1.second].negativeUpdate(ev,injs,lnk1.first->interface[lnk1.second].deps.second);
+		lnk1.first->setLink(lnk1.second,nullptr,0);
+		ev.side_effects.emplace(lnk1);
+	}
+	if(lnk2.first){
+		lnk2.first->interface[lnk2.second].negativeUpdate(ev,injs,lnk2.first->interface[lnk2.second].deps.second);
+		lnk2.first->setLink(lnk2.second,nullptr,0);
+		ev.side_effects.emplace(lnk2);
+	}
+	interface[id].negativeUpdate(ev,injs,interface[id].deps.second);
+	lnk1.first = trgt_node;lnk1.second = trgt_site;
+	trgt_node->interface[trgt_site].negativeUpdate(ev,injs,trgt_node->interface[trgt_site].deps.second);
+	trgt_node->setLink(trgt_site,this,id);
+}
+
+Node::Internal* Node::begin(){
+	return interface;
+}
+Node::Internal* Node::end(){
+	return interface+intfSize;
+}
+
+const SomeValue& Node::getInternalState(small_id id){
+	return interface[id].val;
+}
+const pair<Node*,small_id>& Node::getLinkState(small_id id){
+	return interface[id].link;
+}
+
+pair<matching::InjSet*,matching::InjSet*>& Node::getLifts(small_id site){
+	return interface[site].deps;
+}
+
+
+
+/*********** MultiNode *****************/
+
+MultiNode::MultiNode(unsigned n,pop_size pop) :
+		population(pop),count(0) {
+	cc = new SubNode*[n];
+}
+
+pop_size MultiNode::getPopulation() const {
+	return population;
+}
+
+small_id MultiNode::add(SubNode* node){
+	cc[count] = node;
+	//count++;
+	return count++;//count-1;//test
+}
+
+void MultiNode::dec(EventInfo& ev) {
+	for(small_id i = 0; i < count; i++){
+		ev.new_cc.emplace(cc[i],nullptr);
+	}
+	population--;
+	if(!population){
+		delete this;
+	}
+}
+
+/************ SubNode ******************/
+
+SubNode::SubNode(const pattern::Signature& sign,MultiNode& multi) :
+		Node(sign),cc(multi) {
+	multiId = multi.add(this);
+};
+SubNode::~SubNode(){}
+
+pop_size SubNode::getCount() const {
+	return cc.getPopulation();
+}
+
+void SubNode::removeFrom(EventInfo& ev,matching::InjSet* injs,SiteGraph& graph){
+	cc.dec(ev);
+	ev.new_cc[this] = this;
+}
+
+void SubNode::changeIntState(EventInfo& ev,matching::InjSet* injs,small_id id,small_id value){
+	cc.dec(ev);
+	auto new_node = new Node(*this,ev.new_cc);
+	new_node->setState(id,value);
+	ev.new_cc.at(this) = new_node;
+}
+void SubNode::unbind(EventInfo& ev,matching::InjSet* injs,small_id id){
+	//unbind must be inside cc
+	cc.dec(ev);
+	auto node1 = new Node(*this,ev.new_cc);
+	auto node2 = new Node(*this->interface[id].link.first,ev.new_cc);
+	ev.new_cc.at(this) = node1;
+	ev.new_cc.at(this->interface[id].link.first) = node2;
+}
+void SubNode::bind(EventInfo& ev,matching::InjSet* injs,small_id id,Node* trgt_node,small_id trgt_site){
+	cc.dec(ev);
+	auto new_node = new Node(*this,ev.new_cc);
+	ev.new_cc.at(this) = new_node;
+	try{
+		auto new_trgt_node = ev.new_cc.at(trgt_node);
+		if(new_trgt_node){
+			throw invalid_argument("link between 2 sites of same agent, this cannot happen. do not make sense");
+		}
+		else{
+			//link with a node in the same cc
+			new_trgt_node = new Node(*trgt_node,ev.new_cc);
+			Node::bind(ev,injs,id,new_trgt_node,trgt_site);
+		}
+	}
+	catch(std::out_of_range &e){
+		Node::bind(ev,injs,id,trgt_node,trgt_site);
+	}
+
+}
+
+/********** Internal struct ************/
+
+Node::Internal::Internal() : val(small_id(-1)),link(nullptr,0),
+		deps(new matching::InjSet(),new matching::InjSet()){}
+
+Node::Internal::~Internal(){
+	delete deps.first;
+	delete deps.second;
+}
+
+void Node::Internal::negativeUpdate(EventInfo& ev,matching::InjSet* injs){
+	negativeUpdate(ev,deps.first);
+	negativeUpdate(ev,deps.second);
+}
+
+void Node::Internal::negativeUpdate(EventInfo& ev,matching::InjSet* injs,matching::InjSet* deps){
+	for(auto dep_it = deps->begin();dep_it != deps->end();dep_it++){
+		if((*dep_it)->isTrashed())
+			deps->erase(dep_it);
+		else{
+			auto& emb = (*dep_it)->getEmbedding();
+			auto& cc = (*dep_it)->pattern();
+			for(unsigned i = 0; i < emb.size(); i++){
+				auto& mix_ag = cc.getAgent(i);
+				for(auto& id_site : mix_ag){
+					switch(id_site.second.state.t){
+					case BaseExpression::SMALL_ID:
+						if(!id_site.second.isEmptySite())
+							emb[i]->getLifts(id_site.first).first->erase(*dep_it);
+						break;
+					default:
+						break;//TODO other types of values
+					}
+					switch(id_site.second.link_type){
+					case pattern::Pattern::FREE:case pattern::Pattern::BIND:case pattern::Pattern::BIND_TO:
+						emb[i]->getLifts(id_site.first).first->erase(*dep_it);
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			injs[cc.getId()].erase(*dep_it);
+		}
+	}
+}
+
+string Node::toString(const pattern::Environment &env) const {
+	auto& sign = env.getSignature(signId);
+	string s(sign.getName()+"(");
+	for(small_id i = 0; i < intfSize; i++){
+		s += interface[i].toString(sign.getSite(i));
+		try{
+			dynamic_cast<const pattern::Signature::EmptySite&>(sign.getSite(i));
+		}
+		catch(std::bad_cast &e){
+			s += "~";
+			switch(interface[i].val.t){
+			case BaseExpression::FLOAT:
+				s += to_string(interface[i].val.fVal);
+				break;
+			case BaseExpression::INT:
+				s += to_string(interface[i].val.iVal);
+				break;
+			case BaseExpression::SHORT_ID:
+				s += dynamic_cast<const pattern::Signature::LabelSite&>(sign.getSite(i))
+						.getLabel(interface[i].val.shortVal);
+				break;
+			case BaseExpression::SMALL_ID:
+				s += dynamic_cast<const pattern::Signature::LabelSite&>(sign.getSite(i))
+						.getLabel(interface[i].val.smallVal);
+				break;
+			default:
+				s += "?";//TODO exception?
+			}
+		}
+		s += ",";
+	}
+	if(intfSize)
+		s.back() = ')';
+	else
+		s += ")";
+	return s;
+}
+
+string Node::Internal::toString(const pattern::Signature::Site& sit) const {
+	string s(sit.getName());
+	return s;
+}
+
+
+} /* namespace state */
---
+++ src/state/Node.h
@@ -0,0 +1,162 @@
+/*
+ * Node.h
+ *
+ *  Created on: Oct 18, 2017
+ *      Author: naxo
+ */
+
+#ifndef SRC_STATE_NODE_H_
+#define SRC_STATE_NODE_H_
+
+#include <utility>
+#include <map>
+#include <queue>
+#include "../pattern/Signature.h"
+#include "../pattern/Mixture.h"
+//#include "../matching/Injection.h"
+
+namespace matching {
+	class InjSet;
+}
+
+namespace state {
+
+class SiteGraph;
+struct EventInfo;
+
+using namespace std;
+
+class Node {
+public:
+	struct Internal;
+protected:
+	short_id signId;
+	big_id address;
+	Internal *interface;
+	small_id intfSize;
+	virtual ~Node();
+public:
+	Node(short sign_id,short intf_size);
+	Node(const pattern::Signature& sign);
+	Node(const Node& node,map<Node*,Node*>& mask);
+	void alloc(big_id addr);
+	big_id getAddress() const;
+
+	template<typename T>
+	void setState(small_id  site_id,T value);
+	void setLink(small_id site_src,Node* lnk,small_id site_trgt);
+
+
+	virtual void removeFrom(EventInfo& ev,matching::InjSet* injs,SiteGraph& graph);
+	virtual void changeIntState(EventInfo& ev,matching::InjSet* injs,small_id id,small_id value);
+	virtual void unbind(EventInfo& ev,matching::InjSet* injs,small_id id);
+	virtual void bind(EventInfo& ev,matching::InjSet* injs,small_id id,Node* trgt_node,small_id trgt_site);
+
+
+	//inline?? TODO
+	Internal* begin();
+	Internal* end();
+
+	void setCount(pop_size q);
+	virtual pop_size getCount() const;
+
+	short_id getId() const;
+
+	/** \brief Tests whether a site of this node matches with a mixture site.
+	 * Tests if interface[id_site.first] matches with id_site.second.
+	 * Stores in port_list every site_id that needs to match, used later to
+	 * save injections. Also it stores in match_queue every pair
+	 * (to_follow_,Node*) that will be matched later.
+	 */
+	bool test(const pair<small_id,pattern::Mixture::Site>& id_site,
+			std::queue<pair<small_id,Node&> > &match_queue,
+			two<list<Internal*> > &port_list) const;
+
+	//unsafe
+	const SomeValue& getInternalState(small_id);
+	const pair<Node*,small_id>& getLinkState(small_id);
+
+
+
+	pair<matching::InjSet*,matching::InjSet*>& getLifts(small_id site);
+	//DEBUG
+	string toString(const pattern::Environment &env) const;
+
+};
+
+struct Node::Internal {
+	SomeValue val;
+	pair<Node*,small_id> link;
+	//for value, for link //TODO refs????
+	pair<matching::InjSet*,matching::InjSet*> deps;//pointers to accept forward declaration of InjSet
+	Internal();
+	~Internal();
+
+	void negativeUpdate(EventInfo& ev,matching::InjSet* injs);
+	void negativeUpdate(EventInfo& ev,matching::InjSet* injs,matching::InjSet* deps);
+
+
+	string toString(const pattern::Signature::Site& s) const;
+
+};
+
+class MultiNode;
+
+class SubNode : public Node {
+	MultiNode& cc;
+	small_id multiId;
+public:
+	SubNode(const pattern::Signature& sign,MultiNode& multi);
+	~SubNode();
+	pop_size getCount() const override;
+
+
+	void removeFrom(EventInfo& ev,matching::InjSet* injs,SiteGraph& graph) override;
+
+	void changeIntState(EventInfo& ev,matching::InjSet* injs,small_id id,small_id value) override;
+	void unbind(EventInfo& ev,matching::InjSet* injs,small_id id) override;
+	void bind(EventInfo& ev,matching::InjSet* injs,small_id id,Node* trgt_node,small_id trgt_site) override;
+
+	/*bool test(const pair<small_id,pattern::Mixture::Site>& id_site,
+			std::queue<pair<small_id,Node&> > &match_queue,
+			two<list<Internal*> > &port_list) const;*/
+};
+
+class MultiNode {
+	pop_size population;
+
+	SubNode** cc;
+	small_id count;
+
+public:
+	MultiNode(unsigned n,pop_size pop);
+
+	pop_size getPopulation() const;
+
+	small_id add(SubNode* node);
+	void dec(EventInfo& ev);
+};
+
+
+/** \brief Structure that stores the information related to an event.
+ */
+struct EventInfo {
+	//map of emb LHS [cc_id][ag_id]
+	Node*** emb;
+	//map of new_nodes RHS
+	Node** fresh_emb;
+	//node_address,site_id
+	std::set<pair<Node*,small_id> > side_effects;
+	//perturbation_ids
+	std::set<mid_id> pert_ids;
+	//null events
+	small_id warns;
+
+	//new cc derived from multinode
+	map<Node*,Node*> new_cc;
+
+};
+
+} /* namespace state */
+
+#endif /* SRC_STATE_NODE_H_ */

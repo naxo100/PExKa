b59b4cf401a9e268490604b802b37f0ede0210d7
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2017-12-01 12:57:54 -0300

    - Implementation of WarningStack::show()

--- src/util/Warning.cpp
+++ src/util/Warning.cpp
@@ -7,16 +7,27 @@
 
 #include "Warning.h"
 
-WarningStack::WarningStack(){}
 
 Warning::Warning(const std::string& m,const yy::location& l) : msg(m),loc(l){}
 
 Warning::~Warning() {}
 
+const std::string& Warning::what() const{
+	return msg;
+}
+
+
+
 WarningStack WarningStack::stack = WarningStack();
 
 
+WarningStack::WarningStack(){}
 WarningStack& WarningStack::getStack() {
 	return stack;
 }
 
+void WarningStack::show() const {
+	for(auto& warn : *this){
+		std::cout << warn.what() << std::endl;
+	}
+}

9a9f7db8f91fc90dff94b1d3a3728b54a743b4dc
Ignacio Fuenzalida <ignacio.fuenzalidav@gmail.com>
2017-12-12 17:00:16 -0300

    - First attempt to implement plot. Just to allow easy debugging of simulations.

---
+++ src/simulation/Plot.h
@@ -0,0 +1,33 @@
+/*
+ * Plot.h
+ *
+ *  Created on: Dec 12, 2017
+ *      Author: naxo
+ */
+
+#ifndef SRC_SIMULATION_PLOT_H_
+#define SRC_SIMULATION_PLOT_H_
+
+#include <iostream>
+#include <fstream>
+#include "../state/State.h"
+#include "Parameters.h"
+
+namespace simulation {
+
+using namespace std;
+
+class Plot {
+	ofstream file;
+	FL_TYPE nextPoint;
+	FL_TYPE dT;
+public:
+	Plot(const pattern::Environment& env);
+	~Plot();
+
+	void fill(const state::State& state,const pattern::Environment& env);
+};
+
+} /* namespace simulation */
+
+#endif /* SRC_SIMULATION_PLOT_H_ */

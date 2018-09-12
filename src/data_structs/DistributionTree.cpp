8af09eb5b3be48a70f8c12aae60d145b4f4b23cf
naxo100 <ignacio.fuenzalidav@gmail.com>
2018-09-12 15:59:30 -0300

    - New data struct to implement injection sets for valued sites.

---
+++ src/data_structs/DistributionTree.cpp
@@ -0,0 +1,18 @@
+/*
+ * DistributionTree.cpp
+ *
+ *  Created on: Jun 19, 2018
+ *      Author: naxo100
+ */
+
+#include "DistributionTree.h"
+
+DistributionTree::DistributionTree() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DistributionTree::~DistributionTree() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/data_structs/DistributionTree.h
@@ -0,0 +1,17 @@
+/*
+ * DistributionTree.h
+ *
+ *  Created on: Jun 19, 2018
+ *      Author: naxo100
+ */
+
+#ifndef SRC_DATA_STRUCTS_DISTRIBUTIONTREE_H_
+#define SRC_DATA_STRUCTS_DISTRIBUTIONTREE_H_
+
+class DistributionTree {
+public:
+	DistributionTree();
+	virtual ~DistributionTree();
+};
+
+#endif /* SRC_DATA_STRUCTS_DISTRIBUTIONTREE_H_ */

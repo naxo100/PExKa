e54e7105b295d4f8e49332129f204b905e41d7de
naxo100 <ignacio.fuenzalidav@gmail.com>
2019-04-15 13:59:50 -0300

    - Make for release version.

---
+++ Release/sources.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+CC_SRCS := 
+ASM_SRCS := 
+CPP_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+CC_DEPS := 
+C++_DEPS := 
+EXECUTABLES := 
+C_UPPER_DEPS := 
+CXX_DEPS := 
+OBJS := 
+CPP_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src/data_structs \
+src/expressions \
+src/grammar \
+src/grammar/ast \
+src \
+src/matching \
+src/pattern \
+src/pattern/mixture \
+src/simulation \
+src/state \
+src/util \
+
